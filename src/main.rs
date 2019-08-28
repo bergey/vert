8e6de9d23b438d069aaefb89e44e497852cabe2e
Daniel Bergey <bergey@teallabs.org>
2019-08-28 21:28:16 +0000

    turn off line buffering for user input

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use std::collections::VecDeque;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, BufWriter, Write, Read};
+use std::os::unix::io::AsRawFd;
+
+extern crate termios;
 
 fn parse_args(args : &mut VecDeque<String>) -> Option<u64> {
     if let Some(a) = args.get(0) {
@@ -17,17 +20,49 @@ fn parse_args(args : &mut VecDeque<String>) -> Option<u64> {
     }
 }
 
+fn setup_term() -> File {
+    use termios::*;
+
+    // TODO don't open / close device every time
+    let tty = File::open("/dev/tty").unwrap();
+    let mut term = Termios::from_fd(tty.as_raw_fd()).unwrap(); // Unix only
+    // let mut term = termios::tcgetattr(tty.as_raw_fd).unwrap(); // Unix only
+    // Unset canonical mode, so we get characters immediately
+    // Disable local echo
+    term.c_lflag &= !(ICANON | ECHO);
+    tcsetattr(tty.as_raw_fd(), TCSADRAIN, &term).unwrap();
+    tty
+}
+
 fn copy_bounded<R : Read, W : Write>(from :  &mut R, to : &mut W, max_lines : u64 ) {
     let mut reader = BufReader::new(from);
     let mut writer = BufWriter::new(to);
     let mut buffer = vec![];
 
     let mut lines : u64 = 0;
+    const PAGE_LINES : u64 = 30; // TODO from terminal
 
-    while lines < max_lines && reader.read_until(b'\n', &mut buffer).unwrap() > 0 {
+    'files: while lines < max_lines && reader.read_until(b'\n', &mut buffer).unwrap() > 0 {
         writer.write(&buffer).unwrap();
         lines += 1;
         buffer.clear();
+
+        if lines % PAGE_LINES == 0 {
+            // wait for input
+            writer.flush().unwrap();
+            'user: loop {
+                let tty = setup_term();
+                for byte in tty.bytes() {
+                    match byte.unwrap() {
+                        b' ' => break 'user,
+                        b'q' => {
+                            break 'files;
+                        }
+                        _ => ()
+                    }
+                }
+            }
+        }
     }
 }
 
