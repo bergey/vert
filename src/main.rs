5fe2d21090471066a7a2c1b8afa68bd431012159
Daniel Bergey <bergey@teallabs.org>
2019-08-28 00:24:28 +0000

    cat STDIN

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use std::env;
-/* use std::io::{self, Read}; */
+use std::io::{self, Read};
 
-fn main() {
-     let argv : Vec<String> = env::args().collect();
-     match argv.split_first() {
-          Some((_, args)) => println!("{}", args.join(" ")),
-          None => println!("Nothing to echo.")
-     }
+fn main() -> io::Result<()> {
+    let argv : Vec<String> = env::args().collect();
+    let mut buffer = String::new();
+    if let Some((_, args)) = argv.split_first() {
+        if args.len() > 0 {
+            println!("{}", args.join(" ")) // TODO files
+        } else {
+            println!("trying to read from STDIN");
+            let l = io::stdin().read_to_string(&mut buffer)?;
+            println!("read {} bytes", l);
+            println!("{}", buffer);
+        }
+    }
+    Ok(())
 }
