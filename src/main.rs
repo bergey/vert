e167587f3a763dc97b893a6274c29066ab709308
Daniel Bergey <bergey@teallabs.org>
2019-08-28 00:32:59 +0000

    also cat filenames

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use std::env;
 use std::io::{self, Read};
+use std::fs::File;
 
 fn main() -> io::Result<()> {
     let argv : Vec<String> = env::args().collect();
     let mut buffer = String::new();
     if let Some((_, args)) = argv.split_first() {
         if args.len() > 0 {
-            println!("{}", args.join(" ")) // TODO files
+            for filename in args {
+                let mut f = File::open(filename)?;
+                f.read_to_string(&mut buffer)?;
+                println!("{}", buffer);
+            }
         } else {
-            println!("trying to read from STDIN");
-            let l = io::stdin().read_to_string(&mut buffer)?;
-            println!("read {} bytes", l);
+            io::stdin().read_to_string(&mut buffer)?;
             println!("{}", buffer);
         }
     }
